5934516321f7a84e75d0812ece3267c3ee2ddb52
yapyongjing <eugene_jing@hotmail.com>
2025-02-27 17:22:02 +0800

    another version

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,107 +1,20 @@
 <script lang="ts">
-  import { onMount } from "svelte";
   import io from "socket.io-client";
-  import * as mediasoupClient from "mediasoup-client";
+  let videoRef: HTMLVideoElement;
+  let socket = io("http://54.169.97.63:3000");
 
-  let socket: any;
-  let device: mediasoupClient.Device;
-  let producerTransport;
-  let producer;
-  let consumerTransport;
-  let videoElement: HTMLVideoElement;
+  async function startStreaming() {
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+    videoRef.srcObject = stream;
 
-  onMount(() => {
-    socket = io("http://54.169.97.63:3000", {
-      transports: ["websocket"], // Force WebSocket mode
+    socket.emit("getRouterRtpCapabilities", async (rtpCapabilities: any) => {
+      console.log("Router Capabilities:", rtpCapabilities);
+      // You would now use mediasoup-client to send the stream
     });
-    socket.on("connect", () => console.log("Connected to server"));
-  });
-
-  async function startStream() {
-    device = new mediasoupClient.Device();
-    const transportInfo = await new Promise((resolve) =>
-      socket.emit("start-stream", resolve)
-    );
-
-    await device.load({
-      routerRtpCapabilities:
-        transportInfo as mediasoupClient.types.RtpCapabilities,
-    });
-
-    producerTransport = device.createSendTransport(
-      transportInfo as mediasoupClient.types.TransportOptions
-    );
-    producerTransport.on("connect", async ({ dtlsParameters }, callback) => {
-      socket.emit("transport-connect", { dtlsParameters });
-      callback();
-    });
-
-    producerTransport.on(
-      "produce",
-      async ({ kind, rtpParameters }, callback) => {
-        socket.emit(
-          "produce",
-          { kind, rtpParameters },
-          ({ id }: { id: string }) => {
-            callback({ id });
-          }
-        );
-      }
-    );
-
-    const stream = await navigator.mediaDevices.getUserMedia({
-      video: true,
-      audio: true,
-    });
-    videoElement.srcObject = stream;
-
-    const track = stream.getVideoTracks()[0];
-    producer = await producerTransport.produce({ track });
-
-    console.log("Streaming started!");
-  }
-
-  async function startViewing() {
-    const transportInfo = await new Promise((resolve) =>
-      socket.emit("start-stream", resolve)
-    );
-
-    consumerTransport = device.createRecvTransport(
-      transportInfo as mediasoupClient.types.TransportOptions
-    );
-    consumerTransport.on("connect", async ({ dtlsParameters }, callback) => {
-      socket.emit("transport-connect", { dtlsParameters });
-      callback();
-    });
-
-    const { id, producerId, kind, rtpParameters } = await new Promise<{
-      id: string;
-      producerId: string;
-      kind: "audio" | "video";
-      rtpParameters: any;
-    }>((resolve) =>
-      socket.emit(
-        "consume",
-        { rtpCapabilities: device.rtpCapabilities },
-        resolve
-      )
-    );
-
-    const consumer = await consumerTransport.consume({
-      id,
-      producerId,
-      kind,
-      rtpParameters,
-    });
-
-    const stream = new MediaStream();
-    stream.addTrack(consumer.track);
-    videoElement.srcObject = stream;
   }
 </script>
 
-<h1>Live Streaming <button on:click={startViewing}>Start Viewing</button></h1>
-<video bind:this={videoElement} autoplay playsinline>
-  <track kind="captions" src="captions.vtt" srclang="en" label="English" />
+<button on:click={startStreaming}>Start Streaming</button>
+<video bind:this={videoRef} autoplay playsinline>
+  <track kind="captions" src="captions.vtt" srclang="en" label="English">
 </video>
-<button on:click={startStream}>Start Streaming</button>
---
+++ src/routes/watch/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import io from "socket.io-client";
+    import { onMount } from "svelte";
+    let videoRef;
+    let socket = io("http://54.169.97.63:3000");
+
+    onMount(async () => {
+      socket.emit("getRouterRtpCapabilities", async (rtpCapabilities:any) => {
+        console.log("Viewer Connected:", rtpCapabilities);
+        // Use mediasoup-client to receive and play the stream
+      });
+    });
+  </script>
+
+  <video bind:this={videoRef} autoplay playsinline>
+    <track kind="captions" src="captions.vtt" srclang="en" label="English">
+  </video>
